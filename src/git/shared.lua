630b21d79949d3ddb77d04bc85e331ea2d715651
DevDavisNunez <david.nunezaguilera.131@gmail.com>
2018-03-11 07:32:59 -0600

    Typo Fix Version

--- src/git/shared.lua
+++ src/git/shared.lua
@@ -4,6 +4,6 @@ APP_REPO = "DavisDev"
 APP_PROJECT = "AdrenalineStates"
 
 APP_VERSION_MAJOR = 0x01 -- major.minor
-APP_VERSION_MINOR = 0x10
+APP_VERSION_MINOR = 0x20
 
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
